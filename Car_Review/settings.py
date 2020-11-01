6645089538b08283c632bf27536401fdc6ea9eef
root <root@localhost.localdomain>
2020-11-01 23:05:59 +0300

    update

--- Car_Review/settings.py
+++ Car_Review/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'Car_Review.urls'
